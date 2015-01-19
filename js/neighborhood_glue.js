75f957f7b13a2e30718182532b9ee57e71bf1cb0
GabeKagan <gabekagan@live.com>
2015-01-19 14:21:15 -0500

    Minor changes (info windows and labels)
    Because learning.

--- js/neighborhood_glue.js
+++ js/neighborhood_glue.js
@@ -11,15 +11,16 @@ Other stuff
 
 var locationList = [];
 
-//global test
 var map = "";
 
 //Constructs stuff that'll get put in locationList.
-var neighborhoodLocation = function(name, lat, lng) {
+//If we get this from Google Maps API requests, that might help out a bit.
+var neighborhoodLocation = function(name, lat, lng, contentString) {
     var self = this; //I assume this has to be done to hook things into Knockout.js
     self.name = name;
     self.lat = lat;
     self.lng = lng;
+    self.contentString = contentString;
 }
 
 //Knockoutify this?
@@ -34,6 +35,13 @@ var addMarker = function(neighborhoodLocation) {
         position: {lat: this.lat, lng: this.lng,},
     });
     locationMarker.setMap(map);
+
+    var infowindow = new google.maps.InfoWindow({
+        content: neighborhoodLocation.contentString,
+    });
+    google.maps.event.addListener(locationMarker, 'click', function() {
+        infowindow.open(map, locationMarker);
+    });
 }
 
 
@@ -44,18 +52,23 @@ function initialize() {
         };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    //Convert the marker code into its own function
-    locationList[0] = new neighborhoodLocation("Ginger Tree", 42.627021, -71.415069);
-    locationList[1] = new neighborhoodLocation("J. V. Fletcher Library", 42.5670675, -71.4476384);
-    
-    //Make a loop to populate the markers
-    addMarker(locationList[0]);
-    addMarker(locationList[1]);    
+    //Make a function that assembles new locations based on some sort of API request.
+    locationList[0] = new neighborhoodLocation("Ginger Tree", 42.627021, -71.415069, "Blah blah food");
+    locationList[1] = new neighborhoodLocation("J. V. Fletcher Library", 42.5670675, -71.4476384, "Hurp durp books");
+    console.log(locationList);
+    //Add more things to the markers. 
+    for(var i=0;i<locationList.length;++i)
+    {
+        addMarker(locationList[i]);
+    }   
 
-    }
+}
 
 //This is the controller?
 function SearchViewModel() {
+    var self = this;
+    this.test = ko.observable("Use this controller function to grab all the data from the array locationList");
+    this.test2 = ko.observable("Then use HTML/JS to print it out here.");
     this.searchPrompt = ko.observable("Test");
 }
 
